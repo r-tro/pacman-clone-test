c1b640f2a0e55fff5b694e62a669c9637123dd27
13044173 <65929670+13044173@users.noreply.github.com>
2020-09-13 20:07:09 +1000

    top left + top right chunks + pacman movement loop

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -8,8 +8,17 @@ public class LevelGenerator : MonoBehaviour
     public Vector2 StartPoint;
     public int[,] levelMap =
     {
-       //           j
-       //0,1,2,3,4,5,6,7,8,9,10,11,12   
+        /*
+         * 0 – Empty (do not instantiate anything)
+         * 1 - Outside corner (double lined corener piece in orginal game)
+         * 2 - Outside wall (double line in original game)
+         * 3 - Inside corner (single lined corener piece in orginal game)
+         * 4 - Inside wall (single line in orginal game)
+         * 5 - Standard pellet (see Visual Assets above)
+         * 6 - Power pellet (see Visual Assets above)
+         * 7 - A t junction piece for connecting with adjoining regions 
+         */
+ 
         {1,2,2,2,2,2,2,2,2,2,2,2,2,7},//0
         {2,5,5,5,5,5,5,5,5,5,5,5,5,4},//1
         {2,5,3,4,4,3,5,3,4,4,4,3,5,4},//2
@@ -25,6 +34,8 @@ public class LevelGenerator : MonoBehaviour
         {0,0,0,0,0,2,5,4,4,0,3,4,4,0},//12
         {2,2,2,2,2,1,5,3,3,0,4,0,0,0},//13
         {0,0,0,0,0,0,5,0,0,0,4,0,0,0},//14
+       //0,1,2,3,4,5,6,7,8,9,10,11,12,13
+       //             j
     };
 
     // Start is called before the first frame update
@@ -50,119 +61,114 @@ public class LevelGenerator : MonoBehaviour
             {
                 Transform TopLeft = GameObject.Find("TopLeft").transform;
                 GameObject prefab = Resources.Load("tile_" + levelMap[i, j].ToString()) as GameObject;
-                GameObject tile = Instantiate(prefab) as GameObject;
-                tile.transform.position = new Vector3(StartPoint.x + (TileSize * j), StartPoint.y - (TileSize * i), 0);
-                tile.gameObject.tag = "Tile" + (levelMap[i, j].ToString());
-                tile.transform.SetParent(TopLeft);
+                GameObject tileTopLeft = Instantiate(prefab) as GameObject;
+                //Debug.Log("topr left " + tileTopLeft + "levelMap[" + i + "," + j + "]");
+                tileTopLeft.transform.position = new Vector3(StartPoint.x + (TileSize * j), StartPoint.y - (TileSize * i), 0);
+                tileTopLeft.gameObject.tag = "Tile" + (levelMap[i, j].ToString());
+                tileTopLeft.transform.SetParent(TopLeft);
 
                 Transform TopRight = GameObject.Find("TopRight").transform;
-                GameObject tile2 = Instantiate(prefab) as GameObject;
-                tile2.transform.position = new Vector3((StartPoint.x + 13) + (TileSize * j), StartPoint.y - (TileSize * i), 0);
-                tile2.gameObject.tag = "Tile" + (levelMap[i, j].ToString());
-                tile2.transform.SetParent(TopRight);
-                //TopR.transform.localScale = new Vector3(-1, 1, 1);
+                GameObject tileTopRight = Instantiate(prefab) as GameObject;
+                //Debug.Log("topr right " + tileTopRight + "levelMap[" + i + "," + j + "]");
+                tileTopRight.transform.position = new Vector3(     (StartPoint.x + (((levelMap.GetLength(1) * 2) - 1) * TileSize)) - (TileSize * j)    , StartPoint.y - (TileSize * i), 0); // x = (((14 tiles across * 2) - 1) * TileSize))
+                tileTopRight.gameObject.tag = "Tile" + (levelMap[i, j].ToString());
+                tileTopRight.transform.SetParent(TopRight);
 
                 if (levelMap[i, j] == 0) // destroy object if 0
                 {
-                    Destroy(tile);
+                    Destroy(tileTopLeft);
+                    Destroy(tileTopRight);
                 }
-                if (i > 0 && j < levelMap.GetLength(1))
+                if (i > 0 && j < levelMap.GetLength(1)) // check piece rotation if next to certain pieces
                 {
-                    // OUTSIDE CORNERS
-                    if (levelMap[i, j] == 1)
+                    if (levelMap[i, j] == 1) // OUTSIDE CORNERS
                     {
-                        if (j + 1 <= levelMap.GetLength(1) && (levelMap[i, j + 1] == 2 || levelMap[1, j - 1] == 2)) // bottom left (OUTSIDE) corner piece 
+                        if (j + 1 <= levelMap.GetLength(1) && levelMap[i, j] == 1) // Top Right (OUTSIDE) Corner
                         {
-                            tile.transform.Rotate(0f, 0f, 90f);
-                            tile2.transform.Rotate(0f, 0f, 90f);
+                            tileTopLeft.transform.Rotate(0f, 0f, 0f);
+                            tileTopRight.transform.Rotate(0f, 0f, 90f);
                         }
-                        if (j + 1 <= levelMap.GetLength(1) && (levelMap[i+1, j] == 2 && j > 0)) // top right (OUTSIDE) corner piece
+                        if (j + 1 <= levelMap.GetLength(1) && (levelMap[i, j + 1] == 2 || levelMap[1, j - 1] == 2)) // Bottom Left (OUTSIDE) Corner
                         {
-                            tile.transform.Rotate(0f, 0f, -90f);
-                            tile2.transform.Rotate(0f, 0f, -90f);
+                            tileTopLeft.transform.Rotate(0f, 0f, 90f);
+                            tileTopRight.transform.Rotate(0f, 0f, 90f);
                         }
-                        if (j + 1 <= levelMap.GetLength(1) && (levelMap[i-1, j] == 2 && j > 0)) // bottom right (OUTSIDE) corner piece
+                        if (j + 1 <= levelMap.GetLength(1) && (levelMap[i + 1, j] == 2 && j > 0)) // Top Right (OUTSIDE) cCorner
                         {
-                            tile.transform.Rotate(0f, 0f, 180f);
-                            tile2.transform.Rotate(0f, 0f, 180f);
+                            tileTopLeft.transform.Rotate(0f, 0f, -90f);
+                            tileTopRight.transform.Rotate(0f, 0f, -90f);
                         }
-                        else
-                        { 
-                            Debug.Log("out of bounds" + levelMap[i, j]); 
+                        if (j + 1 <= levelMap.GetLength(1) && (levelMap[i - 1, j] == 2 && j > 0)) // Bottom Right (OUTSIDE) Corner
+                        {
+                            tileTopLeft.transform.Rotate(0f, 0f, 180f);
+                            tileTopRight.transform.Rotate(0f, 0f, 0f);
                         }
                     }
-                    // OUTSIDE CORNERS
-                    // OUTSIDE WALLS
-                    if (levelMap[i, j] == 2)
+                    if (levelMap[i, j] == 2) // OUTSIDE WALLS
                     {
-                        if (j + 1 <= levelMap.GetLength(1) && (levelMap[i+1, j] == 2 || levelMap[i-1, j] == 2 )) // vertical outside wall if there is another wall next to it
+                        if (j + 1 <= levelMap.GetLength(1) && (levelMap[i + 1, j] == 2 || levelMap[i - 1, j] == 2)) // Vertical Wall
                         {
-                            tile.transform.Rotate(0f, 0f, 90f);
-                            tile2.transform.Rotate(0f, 0f, 90f);
-                        }
-                        else
-                        {
-                            Debug.Log("out of bounds" + levelMap[i, j]);
+                            tileTopLeft.transform.Rotate(0f, 0f, 90f);
+                            tileTopRight.transform.Rotate(0f, 0f, 90f);
                         }
                     }
-                    // OUTSIDE WALLS
-                    // INSIDE CORNERS
-                    if (levelMap[i, j] == 3)
+                    if (levelMap[i, j] == 3) // INSIDE CORNERS
                     {
-                        if (j + 1 <= levelMap.GetLength(1) && ((levelMap[i, j-1] == 3 || levelMap[i, j-1] == 4) && j > 0)) // top right (INSIDE) corner piece
+                        if (j + 1 <= levelMap.GetLength(1) && levelMap[i, j] == 3) // Top Left (INSIDE) Corner
                         {
-                            tile.transform.Rotate(0f, 0f, -90f);
-                            tile2.transform.Rotate(0f, 0f, -90f);
+                            tileTopRight.transform.Rotate(0f, 0f, 0f);
+                            tileTopRight.transform.Rotate(0f, 0f, -90f);
                         }
-                        if (j + 1 <= levelMap.GetLength(1) && ((levelMap[i-1, j] == 3 || levelMap[i-1, j] == 4) && levelMap[i+1, j] != 4)) // bottom left (INSIDE) corner piece
+                        if (j + 1 <= levelMap.GetLength(1) && ((levelMap[i, j - 1] == 3 || levelMap[i, j - 1] == 4) && j > 0)) // Top Right (INSIDE) Corner
                         {
-                            tile.transform.Rotate(0f, 0f, 90f);
-                            tile2.transform.Rotate(0f, 0f, 90f);
+                            tileTopLeft.transform.Rotate(0f, 0f, -90f);
+                            tileTopRight.transform.Rotate(0f, 0f, 90f);
                         }
-                        if (j + 1 <= levelMap.GetLength(1) && ((levelMap[i, j-1] == 3 || levelMap[i, j-1] == 4) && levelMap[i-1, j] != 5)) // bottom right (INSIDE) corner piece
+                        if (j + 1 <= levelMap.GetLength(1) && ((levelMap[i - 1, j] == 3 || levelMap[i - 1, j] == 4) && levelMap[i + 1, j] != 4)) // Bottom Left (INSIDE) corner
                         {
-                            tile.transform.Rotate(0f, 0f, 180f);
-                            tile2.transform.Rotate(0f, 0f, 180f);
+                            tileTopLeft.transform.Rotate(0f, 0f, 90f);
+                            tileTopRight.transform.Rotate(0f, 0f, 270f);
                         }
-                        if ((i == 10 && j == 8) || (i == 9 && j == 8))
+                        if (j + 1 <= levelMap.GetLength(1) && ((levelMap[i, j - 1] == 3 || levelMap[i, j - 1] == 4) && levelMap[i - 1, j] != 5)) // Bottom Right (INSIDE) corner
                         {
-                            tile.transform.Rotate(0f, 0f, -90f);
-                            tile2.transform.Rotate(0f, 0f, -90f);
+                            tileTopLeft.transform.Rotate(0f, 0f, 180f);
+                            tileTopRight.transform.Rotate(0f, 0f, -180f);
                         }
-                        if (i == 7 && j == 13)
+                        if (levelMap[i, j] == 3 && (i == 10 && j == 8) || (i == 9 && j == 8)) // Inside Corner (out of bounds manual adjustment)
                         {
-                            tile.transform.Rotate(0f, 0f, -180f);
-                            tile2.transform.Rotate(0f, 0f, -180f);
+                            tileTopLeft.transform.Rotate(0f, 0f, -90f);
+                            tileTopRight.transform.Rotate(0f, 0f, 90f);
                         }
-                        else
+                        if (levelMap[i, j] == 3 && (i == 7 && j == 13)) // Inside Corner (out of bounds manual adjustment)
                         {
-                            Debug.Log("out of bounds" + levelMap[i, j]);
-                        }
-
-                    }
-                    // INSIDE CORNERS
-                    // INSIDE WALLS
-                    if (levelMap[i, j] == 4)
-                    {
-                        if (j + 1 <= levelMap.GetLength(1) && (levelMap[i,j-1] == 4 || levelMap[i, j-1] == 3)) // vertical inside wall if there is another wall next to it
-                        {
-                            tile.transform.Rotate(0f, 0f, 90f);
-                            tile2.transform.Rotate(0f, 0f, 90f);
-                        }
-                        if ((i == 8 || i == 7 || i == 11 || i == 12) && j == 8) //
-                        {
-                            tile.transform.Rotate(0f, 0f, -90f);
-                            tile2.transform.Rotate(0f, 0f, -90f);
-                        }
-                        else
-                        {
-                            Debug.Log("out of bounds" + levelMap[i, j]);
+                            tileTopLeft.transform.Rotate(0f, 0f, -180f);
+                            tileTopRight.transform.Rotate(0f, 0f, -180f);
                         }
                     }
-                    // INSIDE WALLS
+                    if (levelMap[i, j] == 4) // INSIDE WALLS
+                    {
+                        if (j + 1 <= levelMap.GetLength(1) && (levelMap[i, j - 1] == 4 || levelMap[i, j - 1] == 3)) // Vertical Inside Wall
+                        {
+                            tileTopLeft.transform.Rotate(0f, 0f, 90f);
+                            tileTopRight.transform.Rotate(0f, 0f, 90f);
+                        }
+                        if (levelMap[i, j] == 4 && (i == 8 || i == 7 || i == 11 || i == 12) && j == 8) // Vertical Inside Wall (out of bounds manual adjustment)
+                        {
+                            tileTopLeft.transform.Rotate(0f, 0f, -90f);
+                            tileTopRight.transform.Rotate(0f, 0f, -90f);
+                        }
+                    }
+                }
+                else
+                {
+                    if (levelMap[i, j] == 1 && (i == 0 && j == 0)) // levelMap[0,0];
+                    {
+                        Debug.Log("topr right " + tileTopRight + "levelMap[" + i + "," + j + "]");
+                        tileTopLeft.transform.Rotate(0f, 0f, 0f);
+                        tileTopRight.transform.Rotate(0f, 0f, -90f);
+                    }  
                 }
             }
-
         }
     }
 }
