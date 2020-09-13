1873e354766f67c0f06e541e94b0dccda81fa54f
13044173 <65929670+13044173@users.noreply.github.com>
2020-09-13 18:27:58 +1000

    8th

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -17,12 +17,12 @@ public class LevelGenerator : MonoBehaviour
         {2,5,3,4,4,3,5,3,4,4,4,3,5,3},//4
         {2,5,5,5,5,5,5,5,5,5,5,5,5,5},//5
         {2,5,3,4,4,3,5,3,3,5,3,4,4,4},//6
-        {2,5,3,4,4,3,5,4,4,5,3,4,4,3},//7
-        {2,5,5,5,5,5,5,4,4,5,5,5,5,4},//8  i
-        {1,2,2,2,2,1,5,4,3,4,4,3,0,4},//9  9,8 == 3
-        {0,0,0,0,0,2,5,4,3,4,4,3,0,3},//10  10,8 == 3
-        {0,0,0,0,0,2,5,4,4,0,0,0,0,0},//11  11,8 == 4
-        {0,0,0,0,0,2,5,4,4,0,3,4,4,0},//12  12,8 == 4
+        {2,5,3,4,4,3,5,4,4,5,3,4,4,3},//7  i
+        {2,5,5,5,5,5,5,4,4,5,5,5,5,4},//8
+        {1,2,2,2,2,1,5,4,3,4,4,3,0,4},//9
+        {0,0,0,0,0,2,5,4,3,4,4,3,0,3},//10
+        {0,0,0,0,0,2,5,4,4,0,0,0,0,0},//11
+        {0,0,0,0,0,2,5,4,4,0,3,4,4,0},//12
         {2,2,2,2,2,1,5,3,3,0,4,0,0,0},//13
         {0,0,0,0,0,0,5,0,0,0,4,0,0,0},//14
     };
@@ -31,8 +31,8 @@ public class LevelGenerator : MonoBehaviour
     void Start()
     {
         Screen.SetResolution(1920, 1080, false); //1920x1080, no fullscreen
-        TileSize = 1;
-        StartPoint = new Vector2(1, 1);
+        TileSize = 0.28f;
+        StartPoint = new Vector2(-3.78f, 4.06f);
         LevelMap();
     }
 
@@ -48,27 +48,24 @@ public class LevelGenerator : MonoBehaviour
         {
             for (int j = 0; j < levelMap.GetLength(1); j++) // columns (x)
             {
-                //Debug.Log("prefab" + prefab + " + tile " +tile);\
                 Transform TopLeft = GameObject.Find("TopLeft").transform;
                 GameObject prefab = Resources.Load("tile_" + levelMap[i, j].ToString()) as GameObject;
-                GameObject tile = Instantiate(prefab, Vector3.zero, Quaternion.identity) as GameObject;
+                GameObject tile = Instantiate(prefab) as GameObject;
                 tile.transform.position = new Vector3(StartPoint.x + (TileSize * j), StartPoint.y - (TileSize * i), 0);
                 tile.gameObject.tag = "Tile" + (levelMap[i, j].ToString());
-                tile.transform.parent = TopLeft;
+                tile.transform.SetParent(TopLeft);
 
-                ////////
-                ///
-
-                /*
                 Transform TopRight = GameObject.Find("TopRight").transform;
-                GameObject tile2 = Instantiate(prefab, Vector3.zero, Quaternion.identity) as GameObject;
+                GameObject tile2 = Instantiate(prefab) as GameObject;
+                tile2.transform.position = new Vector3((StartPoint.x + 13) + (TileSize * j), StartPoint.y - (TileSize * i), 0);
                 tile2.gameObject.tag = "Tile" + (levelMap[i, j].ToString());
-                tile2.transform.parent = TopRight;
-                tile2.transform.position = new Vector3((StartPoint.x + 27) - (TileSize * j), (StartPoint.y) - (TileSize * i), 0);
-                TopRight.gameObject.transform.localScale = new Vector3(-1, 1, 1);
-                */
+                tile2.transform.SetParent(TopRight);
+                //TopR.transform.localScale = new Vector3(-1, 1, 1);
 
-                ////////
+                if (levelMap[i, j] == 0) // destroy object if 0
+                {
+                    Destroy(tile);
+                }
                 if (i > 0 && j < levelMap.GetLength(1))
                 {
                     // OUTSIDE CORNERS
@@ -77,14 +74,17 @@ public class LevelGenerator : MonoBehaviour
                         if (j + 1 <= levelMap.GetLength(1) && (levelMap[i, j + 1] == 2 || levelMap[1, j - 1] == 2)) // bottom left (OUTSIDE) corner piece 
                         {
                             tile.transform.Rotate(0f, 0f, 90f);
+                            tile2.transform.Rotate(0f, 0f, 90f);
                         }
                         if (j + 1 <= levelMap.GetLength(1) && (levelMap[i+1, j] == 2 && j > 0)) // top right (OUTSIDE) corner piece
                         {
                             tile.transform.Rotate(0f, 0f, -90f);
+                            tile2.transform.Rotate(0f, 0f, -90f);
                         }
                         if (j + 1 <= levelMap.GetLength(1) && (levelMap[i-1, j] == 2 && j > 0)) // bottom right (OUTSIDE) corner piece
                         {
                             tile.transform.Rotate(0f, 0f, 180f);
+                            tile2.transform.Rotate(0f, 0f, 180f);
                         }
                         else
                         { 
@@ -98,6 +98,7 @@ public class LevelGenerator : MonoBehaviour
                         if (j + 1 <= levelMap.GetLength(1) && (levelMap[i+1, j] == 2 || levelMap[i-1, j] == 2 )) // vertical outside wall if there is another wall next to it
                         {
                             tile.transform.Rotate(0f, 0f, 90f);
+                            tile2.transform.Rotate(0f, 0f, 90f);
                         }
                         else
                         {
@@ -111,14 +112,27 @@ public class LevelGenerator : MonoBehaviour
                         if (j + 1 <= levelMap.GetLength(1) && ((levelMap[i, j-1] == 3 || levelMap[i, j-1] == 4) && j > 0)) // top right (INSIDE) corner piece
                         {
                             tile.transform.Rotate(0f, 0f, -90f);
+                            tile2.transform.Rotate(0f, 0f, -90f);
                         }
                         if (j + 1 <= levelMap.GetLength(1) && ((levelMap[i-1, j] == 3 || levelMap[i-1, j] == 4) && levelMap[i+1, j] != 4)) // bottom left (INSIDE) corner piece
                         {
                             tile.transform.Rotate(0f, 0f, 90f);
+                            tile2.transform.Rotate(0f, 0f, 90f);
                         }
                         if (j + 1 <= levelMap.GetLength(1) && ((levelMap[i, j-1] == 3 || levelMap[i, j-1] == 4) && levelMap[i-1, j] != 5)) // bottom right (INSIDE) corner piece
                         {
                             tile.transform.Rotate(0f, 0f, 180f);
+                            tile2.transform.Rotate(0f, 0f, 180f);
+                        }
+                        if ((i == 10 && j == 8) || (i == 9 && j == 8))
+                        {
+                            tile.transform.Rotate(0f, 0f, -90f);
+                            tile2.transform.Rotate(0f, 0f, -90f);
+                        }
+                        if (i == 7 && j == 13)
+                        {
+                            tile.transform.Rotate(0f, 0f, -180f);
+                            tile2.transform.Rotate(0f, 0f, -180f);
                         }
                         else
                         {
@@ -133,10 +147,12 @@ public class LevelGenerator : MonoBehaviour
                         if (j + 1 <= levelMap.GetLength(1) && (levelMap[i,j-1] == 4 || levelMap[i, j-1] == 3)) // vertical inside wall if there is another wall next to it
                         {
                             tile.transform.Rotate(0f, 0f, 90f);
+                            tile2.transform.Rotate(0f, 0f, 90f);
                         }
-                        if ((levelMap[i, j] == 4 && ((i == 8 || i == 7 || i == 11 || i == 12) && j == 8))) 
+                        if ((i == 8 || i == 7 || i == 11 || i == 12) && j == 8) //
                         {
                             tile.transform.Rotate(0f, 0f, -90f);
+                            tile2.transform.Rotate(0f, 0f, -90f);
                         }
                         else
                         {
@@ -145,18 +161,8 @@ public class LevelGenerator : MonoBehaviour
                     }
                     // INSIDE WALLS
                 }
-                LevelMapTopRight();
             }
 
         }
-    }
-
-    public void LevelMapTopRight()
-    {
-        //Transform TopRight = GameObject.Find("TopLeft").transform;
-        //Transform duplicate = Instantiate(TopRight);
-        //duplicate.transform.parent = TopRight;
-        //Debug.Log("so far theres " + TopRight);
-        //duplicate.gameObject.transform.localScale = new Vector3(-1, 1, 1);
     }
 }
