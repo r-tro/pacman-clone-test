cad0fb0123481cac0f6cfc953521b2783a284acb
13044173 <65929670+13044173@users.noreply.github.com>
2020-09-14 05:46:21 +1000

    Revert "Feature visual"

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour
-{ 
+{
     [SerializeField]
     private GameObject item;
     //public float speed = 1f;
