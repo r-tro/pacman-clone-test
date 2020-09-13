5a14aaa768ab1136ebbcc8146c6ccc6b68a164d8
13044173 <65929670+13044173@users.noreply.github.com>
2020-09-14 09:17:10 +1000

    Pacman Animation

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,14 +1,17 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour
-{ 
+{
     [SerializeField]
     private GameObject item;
+
     //public float speed = 1f;
     private Tweener tweener;
     private bool InputUsed = false;
+    private Vector2 direction = Vector2.zero;
 
     // for collision <-- use collision detection tags 
 
@@ -23,30 +26,33 @@ public class InputManager : MonoBehaviour
     void Update()
     {
         CheckInput();
-        
+        UpdateOrientation();
     }
 
     void Awake()
     {
-        
+
     }
 
     IEnumerator MoveLoop()
     {
         while (InputUsed == false)
         {
-            tweener.AddTween(item.transform, item.transform.position, new Vector3(item.transform.localPosition.x - (int)0.0, item.transform.localPosition.y - (int)4.0, item.transform.localPosition.z + (int)0.0), (int)1.0); //converted to int for absolute value
-            yield return new WaitForSeconds(1.01f); 
-            tweener.AddTween(item.transform, item.transform.position, new Vector3(item.transform.localPosition.x + (int)11.0, item.transform.localPosition.y + (int)0.0, item.transform.localPosition.z + (int)0.0), (int)2.0);
+            direction = Vector2.right;
+            tweener.AddTween(item.transform, item.transform.position, new Vector3(item.transform.localPosition.x + (int)11.0, item.transform.localPosition.y + (int)0.0, item.transform.localPosition.z + (int)0.0), (int)2.0); //converted to int for absolute value
             yield return new WaitForSeconds(2.01f);
-            tweener.AddTween(item.transform, item.transform.position, new Vector3(item.transform.localPosition.x + (int)0.0, item.transform.localPosition.y  + (int)4.0, item.transform.localPosition.z + (int)0.0), (int)1.0);
+            tweener.AddTween(item.transform, item.transform.position, new Vector3(item.transform.localPosition.x + (int)0.0, item.transform.localPosition.y + (int)-4.0, item.transform.localPosition.z + (int)0.0), (int)1.0);
+            direction = Vector2.down;
             yield return new WaitForSeconds(1.01f);
-            tweener.AddTween(item.transform, item.transform.position, new Vector3(item.transform.localPosition.x - (int)11.0, item.transform.localPosition.y + (int)0.0, item.transform.localPosition.z + (int)0.0), (int)2.0);
+            tweener.AddTween(item.transform, item.transform.position, new Vector3(item.transform.localPosition.x + (int)-11.0, item.transform.localPosition.y + (int)0.0, item.transform.localPosition.z + (int)0.0), (int)2.0);
+            direction = Vector2.left;
             yield return new WaitForSeconds(2.01f);
-            yield return null;
+            tweener.AddTween(item.transform, item.transform.position, new Vector3(item.transform.localPosition.x + (int)0.0, item.transform.localPosition.y + (int)4.0, item.transform.localPosition.z + (int)0.0), (int)1.0);
+            direction = Vector2.up;
+            yield return new WaitForSeconds(1.01f);
         }
         //Debug.Log("not looping, input detected");
-            yield break;
+        yield break;
     }
 
     void CheckInput()
@@ -54,22 +60,50 @@ public class InputManager : MonoBehaviour
         if (Input.GetKeyDown("w")) //up
         {
             InputUsed = true;
+            direction = Vector2.up;
             tweener.AddTween(item.transform, item.transform.position, new Vector3(item.transform.localPosition.x, item.transform.localPosition.y + (int)1.0, item.transform.localPosition.z), (int)1.0);
         }
         if (Input.GetKeyDown("s")) //down
         {
             InputUsed = true;
+            direction = Vector2.down;
             tweener.AddTween(item.transform, item.transform.position, new Vector3(item.transform.localPosition.x, item.transform.localPosition.y - (int)1.0, item.transform.localPosition.z), (int)1.0);
         }
         if (Input.GetKeyDown("a")) //left
         {
             InputUsed = true;
+            direction = Vector2.left;
             tweener.AddTween(item.transform, item.transform.position, new Vector3(item.transform.localPosition.x - (int)1.0, item.transform.localPosition.y, item.transform.localPosition.z), (int)1.0);
         }
         if (Input.GetKeyDown("d")) //right
         {
             InputUsed = true;
+            direction = Vector2.right;
             tweener.AddTween(item.transform, item.transform.position, new Vector3(item.transform.localPosition.x + (int)1.0, item.transform.localPosition.y, item.transform.localPosition.z), (int)1.0);
+        }
+    }
+
+    void UpdateOrientation()
+    {
+        if (direction == Vector2.up)
+        {
+            item.transform.localScale = new Vector3(1, 1, 1);
+            item.transform.localRotation = Quaternion.Euler(0, 0, 90);
+        }
+        else if (direction == Vector2.down)
+        {
+            item.transform.localScale = new Vector3(1, 1, 1);
+            item.transform.localRotation = Quaternion.Euler(0, 0, -90);
+        }
+        else if (direction == Vector2.left)
+        {
+            item.transform.localScale = new Vector3(-1, 1, 1);
+            item.transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+        else if (direction == Vector2.right)
+        {
+            item.transform.localScale = new Vector3(1, 1, 1);
+            item.transform.localRotation = Quaternion.Euler(0, 0, 0);
         }
     }
 }
